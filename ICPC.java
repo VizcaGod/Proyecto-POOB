c5ff81fbf124a5ac4b15a74a599cf4e5c1b8e8ce
unknown <juan.vizcaino@labinfo.is.escuelaing.edu.co>
2022-09-05 10:22:48 -0500

    added shapes code

--- ICPC.java
+++ ICPC.java
@@ -9,6 +9,11 @@ public class ICPC
 {
     // instance variables - replace the example below with your own
     private int x;
+    private String[] intersections;
+    private String[] roads;
+    private String[] signs;
+    private boolean valid;
+    
 
     /**
      * Constructor for objects of class ICPC
@@ -20,24 +25,80 @@ public class ICPC
     }
 
     /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
+     * Añade una interseccion con el color y la ubicacion especificada
      */
     public void addIntersection(String color, int x, int y){
         
     }
+    
+    /**
+     * Añade una ruta con las intersecciones señaladas
+     */
     public void addRoute(String intersectionA, String intersectionB){
         
     }
+    
+    /**
+     * Pone una señal de velocidad con el limite especificado en la interseccion especificada
+     */
     public void putSign(String intersectionA, String intersectionB, int speedLimit){
         
     }
-    public void delIntersection(String color, int x, int y){
+    
+    /**
+     * Elimina la interseccion del color especificado
+     */
+    public void delIntersection(String color){
         
     }
-    public void addIntersection(String color, int x, int y){
+    
+    /**
+     * Elimina el camino con las ubicaciones especificadas
+     */
+    public void delRoad(String locationA, String locationB){
+        
+    }
+    
+    /**
+     * Quita el signo que está en las intersecciones especificadas
+     */
+    public void removeSign(String intersectionA, String intersectionB){
+        
+    }
+    
+    /**
+     * Retorna la lista de las intersecciones actuales
+     */
+    public String[] intersections(){
+       return intersections; 
+    }
+    
+    /**
+     * Hace invisible el mapa
+     */
+    public void makeInvisible(){
+    
+    }
+    
+    /**
+     * Hace visible el mapa
+     */
+    public void makeVisibe(){
+        
+    }
+    
+    /**
+     * 
+     */
+    public void finish(){
+        
+    }
+    
+    /**
+     * Verifica si el mapa es valido
+     */
+    public boolean ok(){
+        return valid;
         
     }
 }

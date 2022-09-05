df76669a81065136170c75473c3ff7f3f5939ede
unknown <juan.vizcaino@labinfo.is.escuelaing.edu.co>
2022-09-05 08:53:40 -0500

    First commit

---
+++ ICPC.java
@@ -0,0 +1,43 @@
+
+/**
+ * Write a description of class ICPC here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class ICPC
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class ICPC
+     */
+    public ICPC()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public void addIntersection(String color, int x, int y){
+        
+    }
+    public void addRoute(String intersectionA, String intersectionB){
+        
+    }
+    public void putSign(String intersectionA, String intersectionB, int speedLimit){
+        
+    }
+    public void delIntersection(String color, int x, int y){
+        
+    }
+    public void addIntersection(String color, int x, int y){
+        
+    }
+}
